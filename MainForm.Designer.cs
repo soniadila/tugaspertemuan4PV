134c04910e43f2868af9a6bba70e758852a93ae2
soniadila <99525698+soniadila@users.noreply.github.com>
2022-03-02 15:40:21 +0700

    Tugas Pertemuan 4 PV

---
+++ MainForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 02/03/2022
+ * Time: 14:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace tugaspertemuan4
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(26, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(119, 26);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Masukkan Nama Anda";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(191, 32);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(301, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(26, 80);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(119, 26);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Masukkan NIM Anda";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(191, 77);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(301, 20);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(26, 128);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(159, 26);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Masukkan KOM/Kelas Anda";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(191, 128);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(301, 20);
+			this.textBox3.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(26, 176);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(119, 26);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Masukkan Mata Kuliah";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(191, 176);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(301, 20);
+			this.textBox4.TabIndex = 7;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.button1.Location = new System.Drawing.Point(140, 255);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(222, 57);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "Klik Saya";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(530, 365);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "tugaspertemuan4";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 02/03/2022
+ * Time: 14:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tugaspertemuan4
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			MessageBox.Show ("Hello World " + this.textBox1.Text + ", NIM Anda : " + this.textBox2.Text + ", KOM Anda : " + this.textBox3.Text + ", Mata Kuliah : " + this.textBox4.Text);	
+		}
+		
+	}
+}
